0e887b79d0a017c1c7722143a7e27620f8960754
Zhiznevski <w.heisenberggg@gmail.com>
2023-12-07 11:40:43 +0300

    feat: add jest configuration

---
+++ __tests__/Header.test.tsx
@@ -0,0 +1,11 @@
+import {render, screen } from '@testing-library/react';
+import Header from '../src/app/Components/Header';
+
+describe('Tests for the Header component', () => {
+    it('Renders the relevant data', () => {
+      render(<Header/>);
+      expect(
+        screen.getByRole('heading', { name: 'hello world' })
+      ).toBeInTheDocument();
+    });
+})
---
+++ src/app/Components/Header.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+return (
+    <>
+    <h1>hello world</h1>
+    </>
+)
+}
+export default Header;
