709178062c4a2ab8fa844218f61f24801cf59a81
Den <info.d1@protonmail.com>
2023-12-09 23:11:56 +0300

    feat: implement sticky header on scroll

--- src/app/Components/header/Header.tsx
+++ src/app/Components/header/Header.tsx
@@ -1,10 +1,28 @@
+'use client';
+
 import Link from 'next/link';
-import './Header.css';
+import { useEffect, useRef } from 'react';
 import { LanguageSelector } from '../language-selector/LanguageSelector';
+import './Header.css';
 
 const Header = () => {
+  const headerRef = useRef<HTMLElement | null>(null);
+
+  const isSticky = () => {
+    const scrollTop = window.scrollY;
+    scrollTop >= 0 && headerRef.current?.classList.add('sticky');
+    scrollTop <= 0 && headerRef.current?.classList.remove('sticky');
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', isSticky);
+    return () => {
+      window.removeEventListener('scroll', isSticky);
+    };
+  });
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Link href="/">
         <h1 className="header-title">GraphiQL</h1>
       </Link>
@@ -35,4 +53,5 @@ const Header = () => {
     </header>
   );
 };
+
 export default Header;
