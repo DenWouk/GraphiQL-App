00069252a5e82761189e757249cd3777ec2fa039
Den <info.d1@protonmail.com>
2023-12-11 21:42:42 +0300

    refactor: return components folder

---
+++ src/app/components/language-selector/LanguageSelector.tsx
@@ -0,0 +1,11 @@
+import './LanguageSelector.css';
+
+export function LanguageSelector() {
+  return (
+    <select className="language-select" name="select">
+      <option defaultValue="EN">EN</option>
+      <option value="BY">BY</option>
+      <option value="RU">RU</option>
+    </select>
+  );
+}
