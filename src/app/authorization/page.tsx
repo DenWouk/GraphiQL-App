85588059c6f378e4c5a163d917dee6037d8e8b23
Den <info.d1@protonmail.com>
2023-12-11 19:47:01 +0300

    chore: restore main branch to original state

--- src/app/authorization/page.tsx
+++
@@ -1,9 +0,0 @@
-import './authorization.css';
-
-export default function Authorization() {
-  return (
-    <main className="main">
-      <h2 className="auth-page-title">Authorization page</h2>
-    </main>
-  );
-}
