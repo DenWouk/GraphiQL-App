a139b8ecccb23bc0e8a90b455da316e21128beed
Den <info.d1@protonmail.com>
2024-01-07 23:46:34 +0300

    delete files

--- src/app/error.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-
-export default function ErrorWrapper({ error }: { error: Error }) {
-  return <h2 className="error-message">Error: {error.message}</h2>;
-}
--- src/utils/getLocalStorage.ts
+++
@@ -1,7 +0,0 @@
-export function getLocalStorage(key: string): string {
-  if (typeof window !== 'undefined') {
-    return localStorage.getItem(key) || '';
-  }
-
-  return '';
-}
